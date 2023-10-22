380130a277c57e3734e5bfa63b66b0763c6a5cc4
jorge <68035175+jorgebandeo@users.noreply.github.com>
2023-10-22 20:26:26 -0300

    Update main.c
    pronto

--- Trabalho M2/main.c
+++ Trabalho M2/main.c
@@ -2,146 +2,324 @@
 #include <stdio.h>
 #include <signal.h>
 #include <time.h>
-
-#ifdef _WIN32 || _WIN64
-#include <Windows.h>
-#else
 #include <unistd.h>
-#endif
-
+#include <string.h>
+#include <termios.h>
+pthread_mutex_t mutex_abs = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_arbeg = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_cinto = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_lvt= PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_motor = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_temperature = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutex_overheating = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t signal_motor = PTHREAD_COND_INITIALIZER;
-pthread_cond_t signal_freio = PTHREAD_COND_INITIALIZER;
-
-int ACCELERATE = 0;
+pthread_cond_t cond_display = PTHREAD_COND_INITIALIZER;
+//Injecao do motor
+int ACCELERATE = 0; 
+//ABS
+int FREIO = 0; 
+//Temperatura do motor
 int TEMPERATURE = 50;
-int OVERHEATING = 1;
+int QUENTE = 0;
+//Equipamentos de suporte a vida
+int BATIDA = 0; //arbeg
+int CINTO = 0; //cinto
+//LVT
+int VIDROS = 0;
+int LUZ = 0;
+int TRNCA = 0;
 
-clock_t ACCELERATE_CLOCK_END = 0, ENGINE_TEMPERATURE_CLOCK_END = 0;
-
-void accelerate()
-{
-    ACCELERATE_CLOCK_END = clock();
-    return;
-}
-
-void monitor_engine_temperature()
-{
-    while (1)
-    {
-        pthread_mutex_lock(&mutex_temperature);
-        if (TEMPERATURE > 99)
-        {
-            ENGINE_TEMPERATURE_CLOCK_END = clock();
-            pthread_mutex_lock(&mutex_overheating);
-            OVERHEATING = 1;
-            pthread_mutex_unlock(&mutex_overheating);
+struct timespec ACCELERATE_CLOCK_END, ENGINE_TEMPERATURE_CLOCK_END, 
+clock_accelerate_start, clock_temperature_start,
+ABS_CLOCK_END ,  ABS_CLOCK_START,//ABS
+ARBEG_CLOCK_END, ARBEG_CLOCK_START,//arbeg
+CINTO_CLOCK_END, CINTO_CLOCK_START,//cinto
+LVT_CLOCK_START, // tranca, luz, vidro
+TRANCA_CLOCK_END, //tranca do carro 
+LUZ_CLOCK_END,//Luz dianteira
+VIDRO_CLOCK_END//vidro, luz, tranca
+;
+void *thread_arbeg(void *arg) {
+    while (1) {
+        usleep(11);
+        if (BATIDA == 1) {
+            
+            clock_gettime(CLOCK_MONOTONIC, &ARBEG_CLOCK_START);
+            usleep(1);
+            usleep(15);
+            clock_gettime(CLOCK_MONOTONIC, &ARBEG_CLOCK_END);
         }
-        else
-        {
-            pthread_mutex_lock(&mutex_overheating);
-            OVERHEATING = 0;
-            pthread_mutex_unlock(&mutex_overheating);
-        }
-        pthread_mutex_unlock(&mutex_temperature);
+        
+        pthread_cond_signal(&cond_display);
+        
     }
+    return NULL;
 }
-
-void thread_motor(void)
-{
-    pthread_t temperature_sensor;
-    pthread_create(&temperature_sensor, NULL, monitor_engine_temperature, NULL);
-
-    while (1)
-    {
-        if (ACCELERATE == 1)
-        {
-            accelerate();
+void *thread_cinto(void *arg) {
+    while (1) {
+        usleep(11);
+        if (CINTO == 1) {
+            
+            clock_gettime(CLOCK_MONOTONIC, &CINTO_CLOCK_START);
+            usleep(1);
+            usleep(15);
+            clock_gettime(CLOCK_MONOTONIC, &CINTO_CLOCK_END);
         }
+        
+        pthread_cond_signal(&cond_display);
+        
     }
+    return NULL;
+}
+void *thread_ltv(void *arg) {
+    while (1) {
+        clock_gettime(CLOCK_MONOTONIC, &LVT_CLOCK_START);
+        usleep(11);
+        if (LUZ == 1) {
+            usleep(1);
+            usleep(15);
+            clock_gettime(CLOCK_MONOTONIC, &LUZ_CLOCK_END);
+            
+        }
+        
+        if (VIDROS){
+            
+            usleep(1);
+            usleep(15);
+            clock_gettime(CLOCK_MONOTONIC, &VIDRO_CLOCK_END);
+        }
+        
+
+        if (TRNCA == 1) {
+            
+            usleep(1);
+            usleep(15);
+            clock_gettime(CLOCK_MONOTONIC, &TRANCA_CLOCK_END);
+        }
+  
+        pthread_cond_signal(&cond_display); 
+        
+    }
+    return NULL;
+}
+void *thread_abs(void *arg) {
+    while (1) {
+        usleep(11);
+        if (FREIO == 1) {
+            
+            clock_gettime(CLOCK_MONOTONIC, &ABS_CLOCK_START);
+            usleep(1);
+            usleep(15);
+            clock_gettime(CLOCK_MONOTONIC, &ABS_CLOCK_END);
+        }
+        
+        pthread_cond_signal(&cond_display);
+        
+    }
+    return NULL;
+}
+void *thread_motor(void *arg) {
+    while (1) {
+        usleep(11);
+        if (ACCELERATE == 1) {
+            
+            clock_gettime(CLOCK_MONOTONIC, &clock_accelerate_start);
+            usleep(1);
+            usleep(15);
+            clock_gettime(CLOCK_MONOTONIC, &ACCELERATE_CLOCK_END);
+        }
+        
+        pthread_cond_signal(&cond_display);
+       
+    }
+    return NULL;
+}
+void *thread_temperature(void *arg) {
+    while (1) {
+        usleep(11);
+        
+        if (TEMPERATURE > 70) {
+            clock_gettime(CLOCK_MONOTONIC, &clock_temperature_start);
+            QUENTE = 1;
+            usleep(1000);
+            usleep(15);
+            clock_gettime(CLOCK_MONOTONIC, &ENGINE_TEMPERATURE_CLOCK_END);
+        } else {
+            QUENTE = 0;
+        }
+        
+        
+        
+        // Sinaliza a thread de exibição para atualizar a tela
+        pthread_cond_signal(&cond_display);
+
+        // Aguarde um curto intervalo antes de verificar novamente
+        // 10 microssegundos
+    }
+    return NULL;
 }
 
-void thread_frenagem(void)
-{
-    // logica do freio
-    pthread_mutex_lock(&mutex_motor);
-    pthread_cond_wait(&signal_motor, &mutex_motor);
+void *display(void *arg) {
+    long max_inj = 0, max_temp = 0, max_abs = 0 ,max_arbeg = 0, max_cinto = 0 , max_luz = 0, max_vidro = 0,max_tranca = 0  ;
+    while (1) {
+        usleep(100000);
+        //pthread_cond_wait(&cond_display, &mutex_motor);
+
+        // Calcula o tempo de resposta em microssegundos para ACCELERATE e TEMPERATURE
+        //tranca
+        long tranca_response_time_us =
+        (TRANCA_CLOCK_END.tv_nsec - LVT_CLOCK_START.tv_nsec) ;
+        if(tranca_response_time_us > max_tranca){
+            max_tranca = tranca_response_time_us;
+        }
+        //vidro 
+        long vidro_response_time_us =
+        (VIDRO_CLOCK_END.tv_nsec - LVT_CLOCK_START.tv_nsec) ;
+        if(vidro_response_time_us > max_vidro){
+            max_vidro = vidro_response_time_us;
+        }
+        //LUZ frontal
+        long luz_response_time_us =
+        (LUZ_CLOCK_END.tv_nsec - LVT_CLOCK_START.tv_nsec) ;
+        if(luz_response_time_us > max_luz){
+            max_luz = luz_response_time_us;
+        }
+        //Cinto de seguranca
+        long cinto_response_time_us =
+        (CINTO_CLOCK_END.tv_nsec - CINTO_CLOCK_START.tv_nsec) ;
+        if(cinto_response_time_us > max_cinto){
+            max_cinto = cinto_response_time_us;
+        }
+        //batida - arbeag
+        long arbeg_response_time_us =
+        (ARBEG_CLOCK_END.tv_nsec - ARBEG_CLOCK_START.tv_nsec) ;
+        if(arbeg_response_time_us > max_arbeg){
+            max_arbeg = arbeg_response_time_us;
+        }
+        //ABS - Freio 
+        long abs_response_time_us =
+        (ABS_CLOCK_END.tv_nsec - ABS_CLOCK_START.tv_nsec) ;
+        if(abs_response_time_us > max_abs){
+            max_abs = abs_response_time_us;
+        }
+
+        long accelerate_response_time_us =
+        (ACCELERATE_CLOCK_END.tv_nsec - clock_accelerate_start.tv_nsec) ;
+        if(accelerate_response_time_us > max_inj){
+            max_inj = accelerate_response_time_us;
+        }
+        
+        long  temperature_response_time_us =
+      
+            (ENGINE_TEMPERATURE_CLOCK_END.tv_nsec - clock_temperature_start.tv_nsec) ;
+        if(temperature_response_time_us > max_temp){
+            max_temp = temperature_response_time_us;
+        }
+        // Limpa a tela e imprime os valores
+        printf("\033[H\033[J");
+        printf("Acelerador: %d - Tempo de resposta: %ld us\n", ACCELERATE, max_inj/1000);
+        printf("Temperatura: %d - Tempo de resposta: %ld us\n", QUENTE, max_temp/1000);
+        printf("Freio: %d - Tempo de resposta: %ld us\n", FREIO, max_abs/1000);
+        printf("Bateu: %d - Tempo de resposta: %ld us\n", BATIDA, max_arbeg/1000);
+        printf("Cinto colocado: %d - Tempo de resposta: %ld us\n", CINTO, max_cinto/1000);
+        printf("Luz: %d - Tempo de resposta: %ld us\n", LUZ, max_luz/1000);
+        printf("Tranca: %d - Tempo de resposta: %ld us\n", TRNCA, max_tranca/1000);
+        printf("Vidro: %d - Tempo de resposta: %ld us\n", VIDROS, max_vidro/1000);
+    }
+    return NULL;
 }
+int main(int argc, char *argv[]) {
+    pthread_attr_t main_attr;
+    pthread_attr_init(&main_attr);
+    struct sched_param main_param;
+    main_param.sched_priority = 99; // Prioridade máxima
+    pthread_attr_setschedparam(&main_attr, &main_param);
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &main_param);
 
-void thread_ESV(void)
-{
-    // logica do equipamentos de suporte a vida
-    pthread_mutex_lock(&mutex_motor);
-    pthread_cond_wait(&signal_motor, &mutex_motor);
-}
+    pthread_t t1, t2, tdisplay,tabs, tarbeg, tcinto,tltv ;
 
-void thread_LVT(void)
-{
-    // logica do LVT
-    pthread_mutex_lock(&mutex_motor);
-    pthread_cond_wait(&signal_motor, &mutex_motor);
-}
+    pthread_attr_t motor_attr;
+    pthread_attr_init(&motor_attr);
+    struct sched_param motor_param;
+    motor_param.sched_priority = 70; // Prioridade de funcao
+    pthread_attr_setschedparam(&motor_attr, &motor_param);
 
-void thread_display(void)
-{
-    // logica do freio
-    pthread_mutex_lock(&mutex_motor);
-    pthread_cond_wait(&signal_motor, &mutex_motor);
-}
+    pthread_create(&t1, &motor_attr, thread_motor, NULL);
 
-void thread_input(void)
-{
-    // logica do freio
-    pthread_mutex_lock(&mutex_motor);
-    pthread_cond_wait(&signal_motor, &mutex_motor);
-}
+    pthread_attr_t default_top;
+    pthread_attr_init(&default_top);
+    struct sched_param top_param;
+    top_param.sched_priority = 50; // Prioridade risco
+    pthread_attr_setschedparam(&default_top, &top_param);
 
-int main(int argc, char *argv[])
-{
-    pthread_t t1, t2, t3, t4, t5, t6;
-    pthread_t thread_temperature_variation;
-    pthread_create(&t1, NULL, thread_motor, NULL);
+    pthread_create(&t2, &default_top, thread_temperature, NULL);
+    pthread_create(&tabs, &default_top, thread_abs, NULL);
+    pthread_create(&tarbeg, &default_top, thread_arbeg, NULL);
 
-    clock_t clock_accelereate_start, clock_temperature_start;
 
-    // Criação das threads
+    pthread_attr_t default_attr;
+    pthread_attr_init(&default_attr);
+    struct sched_param default_param;
+    default_param.sched_priority = 1; // Prioridade padrão
+    pthread_attr_setschedparam(&default_attr, &default_param);
 
-    // pthread_create(&t2, NULL, thread_frenagem, NULL);
-    // pthread_create(&t3, NULL, thread_ESV, NULL);
-    // pthread_create(&t4, NULL, thread_LVT, NULL);
-    // pthread_create(&t5, NULL, thread_display, NULL);
-    // pthread_create(&t6, NULL, thread_input, NULL);
+    pthread_create(&tdisplay, &default_attr, display, NULL);
+    pthread_create(&tcinto, &default_attr, thread_cinto, NULL);
+    pthread_create(&tltv, &default_attr, thread_ltv, NULL);
 
-    while (1)
-    {
-        char *line = getchar();
 
-        if (strcmp(&line, "a") == 0)
-        {
-            printf("Acelerando!!\n");
+
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    char line = ' ';
+    while (line != 'q') {
+        line = getchar();
+        if (line == 'a') {
             pthread_mutex_lock(&mutex_motor);
-            ACCELERATE = 1;
-            clock_accelereate_start = clock();
-            ACCELERATE = 0;
+            ACCELERATE = !ACCELERATE;
             pthread_mutex_unlock(&mutex_motor);
-            printf("Pronto\n");
-            printf("Tempo de resposta: %f \n", (double)(ACCELERATE_CLOCK_END - clock_accelereate_start) / CLOCKS_PER_SEC);
-        }
-        if (strcmp(&line, "t") == 0)
-        {
+        }else if (line == 't') {
             pthread_mutex_lock(&mutex_temperature);
-            TEMPERATURE = 100;
-            clock_temperature_start = clock();
+            if (TEMPERATURE == 100){
+                TEMPERATURE = 50;
+            }else{
+                TEMPERATURE = 100;
+            }
+            // Sinaliza a thread de temperatura para verificar imediatamente
+            pthread_cond_signal(&cond_display);
             pthread_mutex_unlock(&mutex_temperature);
-            printf("Tempo de resposta: %f \n", (double)(ENGINE_TEMPERATURE_CLOCK_END - clock_temperature_start) / CLOCKS_PER_SEC);
+        }else if(line == 'f'){
+            pthread_mutex_lock(&mutex_abs);
+            FREIO = !FREIO;
+            pthread_mutex_unlock(&mutex_abs);
+        }else if(line == 'b'){
+            pthread_mutex_lock(&mutex_arbeg);
+            BATIDA = !BATIDA;
+            pthread_mutex_unlock(&mutex_arbeg);
+        }else if(line == 'c'){
+            pthread_mutex_lock(&mutex_cinto);
+            CINTO = !CINTO;
+            pthread_mutex_unlock(&mutex_cinto);
+        }else if(line == 'j'){
+            pthread_mutex_lock(&mutex_lvt);
+            VIDROS = !VIDROS;
+            pthread_mutex_unlock(&mutex_lvt);
+        }else if(line == 'g'){
+            pthread_mutex_lock(&mutex_lvt);
+            TRNCA = !TRNCA;
+            pthread_mutex_unlock(&mutex_lvt);
+        }else if(line == 'l'){
+            pthread_mutex_lock(&mutex_lvt);
+            LUZ = !LUZ;
+            pthread_mutex_unlock(&mutex_lvt);
         }
-        if (strcmp(&line, "f") == 0)
-        {
-            pthread_cond_signal(&signal_freio);
-        }
+        
     }
 
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    pthread_cancel(t1);
+    pthread_cancel(t2);
+    pthread_cancel(tdisplay);
     return 0;
 }
